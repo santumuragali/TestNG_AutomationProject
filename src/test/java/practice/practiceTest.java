d0df73142cc78c4c78f6251d015435f5e08bf18d
santumuragali <annappamuragali@gmail.com>
2026-03-27 20:12:34 +0530

    added test case

---
+++ src/test/java/practice/practiceTest.java
@@ -0,0 +1,14 @@
+package practice;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class practiceTest {
+
+	@Test
+	void test() {
+		System.out.println("PRINTING TEST RESULT");
+	}
+
+}
